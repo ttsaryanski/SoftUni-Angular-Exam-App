0d8c59c32f90cdbbf3a99bcb609acbcefb1855cc
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-11-27 18:17:22 +0200

    CSS edit

--- Cooking Together/client/Application/src/app/core/header/header.component.ts
+++ Cooking Together/client/Application/src/app/core/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  name: string = 'Tsvetan';
+  isAuthenticated: boolean = true;
 }
