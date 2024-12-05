8816c667041fb44ed2f3b29233352caa54502521
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-12-05 21:57:23 +0200

    Add error handling in home page

--- Cooking Together/client/Application/src/app/home/home.component.ts
+++ Cooking Together/client/Application/src/app/home/home.component.ts
@@ -4,24 +4,42 @@ import { LoaderComponent } from '../shared/loader/loader.component';
 import { RecipesService } from '../recipes/recipes.service';
 import { Recipe } from '../types/recipe';
 import { RecipeComponent } from '../recipes/recipe/recipe.component';
+import { ErrorMsgService } from '../core/error-msg/error-msg.service';
+import { ErrorMsgComponent } from '../core/error-msg/error-msg.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, LoaderComponent, RecipeComponent],
+  imports: [RouterLink, LoaderComponent, RecipeComponent, ErrorMsgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   recipes: Recipe[] = [];
   isLoading: boolean = true;
+  hasError: boolean = false;
 
-  constructor(private recipeService: RecipesService) {}
+  constructor(
+    private recipeService: RecipesService,
+    private errorMsgService: ErrorMsgService
+  ) {
+    this.errorMsgService.apiError$.subscribe((err) => {
+      this.hasError = !!err;
+    });
+  }
 
   ngOnInit(): void {
-    this.recipeService.getTopThreeRecipe().subscribe((recipes) => {
-      this.recipes = recipes;
-      this.isLoading = false;
+    this.recipeService.getTopThreeRecipe().subscribe({
+      next: (recipes) => {
+        this.hasError = false;
+        this.errorMsgService.clearError();
+        this.recipes = recipes;
+        this.isLoading = false;
+      },
+      error: () => {
+        this.hasError = true;
+        this.isLoading = true;
+      },
     });
   }
 }
