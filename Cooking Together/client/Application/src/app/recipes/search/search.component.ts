c08798f06493aeee6f66629ab15745ade9335fd4
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-12-07 23:27:03 +0200

    Add pagination in search page

--- Cooking Together/client/Application/src/app/recipes/search/search.component.ts
+++ Cooking Together/client/Application/src/app/recipes/search/search.component.ts
@@ -23,6 +23,11 @@ export class SearchComponent {
   hasError: boolean = false;
   query: string = '';
 
+  currentPage: number = 1;
+  pageSize: number = 9;
+  totalRecipes: number = 0;
+  totalPages: number = 0;
+
   constructor(
     private recipeService: RecipesService,
     private errorMsgService: ErrorMsgService
@@ -33,18 +38,7 @@ export class SearchComponent {
   }
 
   ngOnInit(): void {
-    this.recipeService.getRecipe().subscribe({
-      next: (recipes) => {
-        this.hasError = false;
-        this.errorMsgService.clearError();
-        this.recipes = recipes;
-        this.isLoading = false;
-      },
-      error: () => {
-        this.hasError = true;
-        this.isLoading = false;
-      },
-    });
+    this.loadRecipes();
   }
 
   search(form: NgForm) {
@@ -52,6 +46,42 @@ export class SearchComponent {
     this.recipeService.getRecipe(undefined, this.query).subscribe((recipes) => {
       this.recipes = recipes;
       this.isLoading = false;
+      this.totalRecipes = recipes.length;
+      this.totalPages = Math.ceil(this.totalRecipes / this.pageSize);
     });
+  }
+
+  loadRecipes(): void {
+    this.isLoading = true;
+    this.recipeService
+      .getAllToPaginated(this.currentPage, this.pageSize)
+      .subscribe({
+        next: (recipes) => {
+          this.hasError = false;
+          this.errorMsgService.clearError();
+          this.recipes = recipes.items;
+          this.totalRecipes = recipes.totalCount;
+          this.totalPages = recipes.totalPages;
+          this.isLoading = false;
+        },
+        error: () => {
+          this.hasError = true;
+          this.isLoading = false;
+        },
+      });
+  }
+
+  nextPage(): void {
+    if (this.currentPage < Math.ceil(this.totalRecipes / this.pageSize)) {
+      this.currentPage++;
+      this.loadRecipes();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadRecipes();
+    }
   }
 }
