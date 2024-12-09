f94deda72b441c53ea46d1f145c13969a5b4aa90
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2024-12-09 20:49:50 +0200

    Edit link in README.md

--- Cooking Together/client/Application/src/app/recipes/recipes.service.spec.ts
+++ Cooking Together/client/Application/src/app/recipes/recipes.service.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 import { RecipesService } from './recipes.service';
 
 describe('RecipesService', () => {
   let service: RecipesService;
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, RecipesService],
+    });
     service = TestBed.inject(RecipesService);
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
--- Cooking Together/client/Application/src/app/user/user.service.spec.ts
+++ Cooking Together/client/Application/src/app/user/user.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClient: HttpClient;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler, UserService],
+    });
     service = TestBed.inject(UserService);
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
